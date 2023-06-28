0e725c939cbbf4d076becdffd66ae76e20120195
max <javoprav@gmail.com>
2023-06-28 16:20:08 +0300

    24.1

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
-
-from vehicle.views import MotorcycleViewSet
+from django.urls import path
+from vehicle.views import MotorcycleViewSet, CarListView, CarCreateAPIView
 
 router = DefaultRouter()
 router.register(r'moto', MotorcycleViewSet, basename='moto')
 
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path('car/', CarListView.as_view(), name='car_list'),
+    path('car/create', CarCreateAPIView.as_view(), name='car_create'),
+              ] + router.urls
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,10 +1,18 @@
 from rest_framework import viewsets, generics
 
-from vehicle.models import Motorcycle
-from vehicle.serializers import MotorcycleSerializers
+from vehicle.models import Motorcycle, Car
+from vehicle.serializers import MotorcycleSerializers, CarSerializers
 
 
 class MotorcycleViewSet(viewsets.ModelViewSet):
     serializer_class = MotorcycleSerializers
     queryset = Motorcycle.objects.all()
-    
+
+
+class CarListView(generics.ListAPIView):
+    serializer_class = CarSerializers
+    queryset = Car.objects.all()
+
+
+class CarCreateAPIView(generics.CreateAPIView):
+    serializer_class = CarSerializers
