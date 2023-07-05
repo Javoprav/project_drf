933503fdb0507fd33c5da17e39172755f69cfa4e
max <javoprav@gmail.com>
2023-07-05 14:49:39 +0300

    ch url

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -9,9 +9,9 @@ router.register(r'moto', MotorcycleViewSet, basename='moto')
 urlpatterns = [
     # Cars
     path('car/', CarListView.as_view(), name='car_list'),
-    path('car/create/', CarCreateAPIView.as_view(), name='car_create'),
+    path('car/create', CarCreateAPIView.as_view(), name='car_create'),
 
     # Milage
-    path('milage/create/', MilageCreateAPIView.as_view(), name='milage_create'),
-    path('milage/moto/', MilageMotoListAPIView.as_view(), name='milage_moto_list'),
+    path('milage/create', MilageCreateAPIView.as_view(), name='milage_create'),
+    path('milage/moto', MilageMotoListAPIView.as_view(), name='milage_moto_list'),
 ] + router.urls
