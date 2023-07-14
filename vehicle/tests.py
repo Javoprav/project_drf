6b24f1a59cb5077e2e5a4f1224f70b2468544fef
max <javoprav@gmail.com>
2023-07-14 19:18:50 +0300

    ch CarTestCase

--- vehicle/tests.py
+++ vehicle/tests.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 
 class CarTestCase(APITestCase):
+    """Необходимо обложить тестами один эндпоинт создания сущности и один эндпоинт отображения списка или одной \
+     сущности.
+    Посчитать покрытие тестами."""
     def setUp(self) -> None:
         # Подготовка данных перед каждым тестом
         self.user = User(username='max', is_staff=True)
@@ -13,13 +16,14 @@ class CarTestCase(APITestCase):
         response = self.client.post('/api/token/', {"username": "max", "password": "123"})
         self.access_token = response.json().get('access')
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+        self.test_model_name = 'audi a100'
 
     def test_car_create(self):
-        response = self.client.post('/vehicle/car/create/', {'model': 'audi a100'})
+        response = self.client.post('/vehicle/car/create/', {'model': self.test_model_name})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_car(self):
         self.test_car_create()
         response = self.client.get(f'/vehicle/car/1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json(), {'id': 1, 'model': 'audi a100', 'year': 1900, 'owner': None})
+        self.assertEqual(response.json(), {'id': 1, 'model': self.test_model_name, 'year': 1900, 'owner': None})
